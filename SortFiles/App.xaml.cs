47a5b63bac5e3a73f0a073ba1308b3bec12ca4d1
dNmmer <mail@dnmer.ru>
2025-11-24 00:36:55 +0300

    Первая сборка

--- SortFiles/App.xaml.cs
+++ SortFiles/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Configuration;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -7,7 +7,6 @@ namespace SortFiles;
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
 }
-
